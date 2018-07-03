697c2f91031ab99fbf1bcb76974c2c18a381f9c6
sufuijk <sufuijk@gmail.com>
2018-07-03 09:02:37 +0700

    update feature

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$('li > a').click(function() {
-    $('li').removeClass();
-    $(this).parent().addClass('active');
+$(document).ready(function(){
+	$("li > a").click(
+	    function(event) {
+	      $('li').removeClass('active');
+	      $(this).addClass('active');
+	    }
+	);
 });
-
-alert("hehe");
