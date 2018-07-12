dfde27317bfd869fa943bb8f4799f22546c92445
sufuijk <sufuijk@gmail.com>
2018-07-12 15:53:08 +0700

    update comment facebook

--- _site/js/facebook.js
+++ _site/js/facebook.js
@@ -2,6 +2,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=1650677965007066&autoLogAppEvents=1';
+  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=1874642439497818&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
